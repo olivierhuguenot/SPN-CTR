d18b4d42408468cb126189f65f3218f93c77e3dc
michelscherer <michel.scherer@students.fhnw.ch>
2025-03-21 19:28:21 +0100

    add sInverse / permutation / permutationInverse arrays

--- src/main/java/SPNmodeCTR.java
+++ src/main/java/SPNmodeCTR.java
@@ -10,6 +10,12 @@ public class SPNmodeCTR {
     final static private int[] sBox = {14, 4, 13, 1, 2, 15,
             11, 8, 3, 10, 6, 12, 5, 9, 0, 7};
 
+    final static private int[] sInverse = {14, 3, 4, 8, 1, 12, 10, 15, 7, 13, 9, 6, 11, 2, 5};
+
+    final static private int[] permutation = {0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15};
+
+    final static private int[] permutationInverse = {0, 4, 8, 12, 1, 5, 9, 10, 14, 3, 7, 11, 15};
+
     // CTR Algorithm
 
     public static String ctrAlgorithm(String x, String key) {
